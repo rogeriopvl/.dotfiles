8addc321c0f8eda5763b9fc40a90fd57acc4c28b
rogeriopvl <rogeriopvl@gmail.com>
2013-07-14 19:53:55 +0100

    Added install script to setup symlinks

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+ln -s .dotfiles/tmux.conf $HOME/.tmux.conf
+ln -s .dotfiles/zshrc $HOME/.zshrc
+ln -s .dotfiles/gitconfig $HOME/.gitconfig
+ln -s .dotfiles/gitignore_global $HOME/.gitignore_global

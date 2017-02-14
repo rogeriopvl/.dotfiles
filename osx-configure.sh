6915b1ff97d5e8b43d6f15ad29c05a919e843888
rogeriopvl <rogeriopvl@gmail.com>
2017-02-14 02:36:33 +0000

    add more osx configurations

--- osx-configure.sh
+++ osx-configure.sh
@@ -8,3 +8,21 @@ defaults write NSGlobalDomain KeyRepeat -int 1
 # disable Chrome two finger swipe
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
 defaults write com.google.Chrome.canary AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+# Follow the keyboard focus while zoomed in
+defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
+
+# Show the ~/Library folder
+chflags nohidden ~/Library
+
+# Disable Dashboard
+defaults write com.apple.dashboard mcx-disabled -bool true
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dock dashboard-in-overlay -bool true
+
+# disable automatically rearrange spaces based on recent use
+defaults write com.apple.dock mru-spaces -bool false

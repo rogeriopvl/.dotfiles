cc45234593615ed9d931d676368824d2a53ac9e1
rogeriopvl <rogeriopvl@gmail.com>
2019-10-17 09:15:18 +0100

    create empty secrets env file to avoid errors

--- install.sh
+++ install.sh
@@ -6,3 +6,6 @@ ln -s .dotfiles/zshrc $HOME/.zshrc
 ln -s .dotfiles/zshenv $HOME/.zshenv
 ln -s .dotfiles/gitconfig $HOME/.gitconfig
 ln -s .dotfiles/gitignore_global $HOME/.gitignore_global
+
+# create the empty file just to avoid errors
+touch $HOME/.secretsenv

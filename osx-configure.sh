d5fd0c69d754150803923b7ad5e009ec8edfcc7e
rogeriopvl <rogeriopvl@gmail.com>
2019-10-17 11:27:21 +0100

    improve osx configuration script

--- osx-configure.sh
+++ osx-configure.sh
@@ -18,6 +18,12 @@ defaults write com.apple.universalaccess closeViewZoomFollowsFocus -bool true
 # Show the ~/Library folder
 chflags nohidden ~/Library
 
+# Show the /Volumes folder
+chflags nohidden /Volumes
+
+# Don’t show recent applications in Dock
+defaults write com.apple.dock show-recents -bool false
+
 # Disable Dashboard
 defaults write com.apple.dashboard mcx-disabled -bool true
 
@@ -29,3 +35,26 @@ defaults write com.apple.dock mru-spaces -bool false
 
 # enable key repeat in vscode
 defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+
+# Hot corners
+# Possible values:
+#  0: no-op
+#  2: Mission Control
+#  3: Show application windows
+#  4: Desktop
+#  5: Start screen saver
+#  6: Disable screen saver
+#  7: Dashboard
+# 10: Put display to sleep
+# 11: Launchpad
+# 12: Notification Center
+# 13: Lock Screen
+# Top left screen corner → Mission Control
+defaults write com.apple.dock wvous-tl-corner -int 0
+defaults write com.apple.dock wvous-tl-modifier -int 0
+# Top right screen corner → Desktop
+defaults write com.apple.dock wvous-tr-corner -int 4
+defaults write com.apple.dock wvous-tr-modifier -int 0
+# Bottom left screen corner → Start screen saver
+defaults write com.apple.dock wvous-bl-corner -int 5
+defaults write com.apple.dock wvous-bl-modifier -int 0

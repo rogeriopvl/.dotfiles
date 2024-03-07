906769c89f08ff4a1dce24d7725cf8b00b24cfbe
rogeriopvl <rogerio@hey.com>
2024-03-07 11:05:08 +0000

    chore(nvim): add neorg and update plugins

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,6 +4,8 @@
 -- Add any additional options here
 vim.opt.relativenumber = false
 vim.opt.colorcolumn = "80"
+vim.opt.wrap = true
+vim.opt.textwidth = 85
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics,

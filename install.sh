b01ea52a230900fa838ad9b1ce267c674fa69bbc
rogeriopvl <rogerio@hey.com>
2024-03-21 15:07:54 +0000

    chore: update install script

--- install.sh
+++ install.sh
@@ -1,18 +1,31 @@
 #!/bin/bash -e
 
+# create the empty file just to avoid errors
+touch $HOME/.secretsenv
+
+echo "⏳installing homebrew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo "✅ homebrew"
+
+echo "⏳installing oh-my-zsh..."
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+echo "✅ oh-my-zsh"
+
+echo "⏳creating dotfiles symlinks..."
 ln -s .dotfiles/tmux.conf $HOME/.tmux.conf
 ln -s .dotfiles/zshrc $HOME/.zshrc
 ln -s .dotfiles/zshenv $HOME/.zshenv
 ln -s .dotfiles/gitconfig $HOME/.gitconfig
 ln -s .dotfiles/gitignore_global $HOME/.gitignore_global
 
-# create the empty file just to avoid errors
-touch $HOME/.secretsenv
-
-# NEOVIM
+# create ~/.config if it doesn't exist
 if [ ! -d "$HOME/.config" ]; then
-	echo "Directory $HOME/.config does not exist. Creating it..."
+	echo "⚠️ directory $HOME/.config does not exist. Creating it..."
 	mkdir "$HOME/.config"
 fi
 
+ln -s .dotfiles/alacritty $HOME/.config/alacritty
 ln -s .dotfiles/nvim $HOME/.config/nvim
+ln -s .dotfiles/yabai $HOME/.config/yabai
+ln -s .dotfiles/skhd $HOME/.config/skhd
+echo "✅ dotfiles"

01cf50182dad328f97df1fa421903a9dc4be3173
rogeriopvl <rogerio@hey.com>
2024-05-15 11:07:26 +0100

    chore(wezterm): use Menlo font

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -7,11 +7,10 @@ local config = wezterm.config_builder()
 
 config.colors = colors
 config.window_frame = window_frame
-config.font = wezterm.font("FiraCode Nerd Font")
+config.font = wezterm.font("Menlo", { weight = "Regular" })
 config.font_size = 13.0
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.bold_brightens_ansi_colors = true
-config.color_scheme = "Tokyo Night Storm"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.window_padding = {

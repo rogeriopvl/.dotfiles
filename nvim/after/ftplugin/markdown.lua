d0694765ef42c4880b9d3e2c74c93549f1b09b03
rogeriopvl <rogerio@hey.com>
2024-05-20 13:10:35 +0100

    chore: update nvim config and alacritty

--- nvim/after/ftplugin/markdown.lua
+++
@@ -1,7 +0,0 @@
--- for some reason, the context is not working properly in some markdown files,
--- so this should be a temporary workaround.
-vim.api.nvim_command("TSContextDisable")
-
--- trying this for now, I don't like how the markdown highlighter hiddes the
--- markdown syntax.
-vim.api.nvim_command("set conceallevel=0")
--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 -- Autocmds are automatically loaded on the VeryLazy event
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
+
+-- disable inlay hints, they take up space and are distracting
+vim.api.nvim_create_autocmd("LspAttach", {
+  callback = function()
+    vim.lsp.inlay_hint.enable(false)
+  end,
+})

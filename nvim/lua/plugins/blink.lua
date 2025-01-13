e12c89e382cffea2f82e758bb3a75479f78c2869
rogeriopvl <rogerio@hey.com>
2025-01-13 18:18:54 +0000

    chore(nvim): improve blink.cmp super-tab behavior

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,6 +1,31 @@
 return {
   "saghen/blink.cmp",
   opts = {
-    keymap = { preset = "super-tab" },
+    keymap = {
+      preset = "super-tab",
+      ["<Tab>"] = {
+        function(cmp)
+          if not cmp.is_visible() then
+            return cmp.show()
+          end
+
+          if cmp.snippet_active() then
+            return cmp.accept()
+          else
+            return cmp.select_and_accept()
+          end
+        end,
+        "snippet_forward",
+        "fallback",
+      },
+    },
+    completion = {
+      menu = {
+        auto_show = false,
+      },
+      ghost_text = {
+        enabled = false,
+      },
+    },
   },
 }

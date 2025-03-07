aeb8fd24d31b26c01de0d33b97c07b31fe284926
rogeriopvl <rogerio@hey.com>
2025-03-07 14:56:07 +0000

    chore(nvim): copilot chat set default model to claude3.7

--- nvim/lua/plugins/copilot-chat.lua
+++ nvim/lua/plugins/copilot-chat.lua
@@ -7,7 +7,7 @@ return {
     },
     build = "make tiktoken", -- Only on MacOS or Linux
     opts = {
-      model = "claude-3.5-sonnet",
+      model = "claude-3.7-sonnet",
       mappings = {
         reset = {
           insert = "<C-r>",

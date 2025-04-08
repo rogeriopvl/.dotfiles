9949fda0f87d75fd9926b0304375470eda4f8fc9
rogeriopvl <rogerio@hey.com>
2025-04-08 15:22:43 +0100

    chore(nvim): improve conform and lsp settings

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -12,22 +12,13 @@ return {
     },
     setup = {
       eslint = function()
-        vim.api.nvim_create_autocmd("BufWritePre", {
-          callback = function(event)
-            if not require("lazyvim.util").format.enabled() then
-              -- exit early if autoformat is not enabled
-              return
-            end
-
-            local client = vim.lsp.get_clients({ bufnr = event.buf, name = "eslint" })[1]
-            if client then
-              local diag = vim.diagnostic.get(event.buf, { namespace = vim.lsp.diagnostic.get_namespace(client.id) })
-              if #diag > 0 then
-                vim.cmd("EslintFixAll")
-              end
-            end
-          end,
-        })
+        require("lazyvim.util").lsp.on_attach(function(client)
+          if client.name == "eslint" then
+            client.server_capabilities.documentFormattingProvider = true
+          elseif client.name == "tsserver" then
+            client.server_capabilities.documentFormattingProvider = false
+          end
+        end)
       end,
     },
   },
